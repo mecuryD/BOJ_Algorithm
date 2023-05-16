a73f03e83a1fb8f94b8d4619e98df858ecbc78af
Bugwang Choe <69384652+mecuryD@users.noreply.github.com>
2023-05-17 02:48:34 +0900

    [Silver III] Title: 수리공 항승, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.py
@@ -0,0 +1,21 @@
+import sys
+n,l = map(int,sys.stdin.readline().split())
+pos = list(map(int, sys.stdin.readline().rstrip().split()))
+pos.sort()
+
+# greedy
+count = 1
+tape_start = pos[0] - 0.5
+tape_end = pos[0] - 0.5 + l
+
+for i in range(1,n) :
+    if tape_end >= pos[i]+0.5 :
+        continue
+    elif tape_end == pos[i] :
+        tape_start = pos[i] # new tape
+    else :
+        tape_start = pos[i] - 0.5 # new tape
+    
+    tape_end = tape_start + l
+    count += 1
+print(count)

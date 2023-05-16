2aadd3132648e133e8b1a77dca4f92ecf711ee5b
Bugwang Choe <69384652+mecuryD@users.noreply.github.com>
2023-05-17 03:35:04 +0900

    [Silver III] Title: 수리공 항승, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1449. 수리공 항승/수리공 항승.py
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.py
@@ -9,11 +9,8 @@ tape_start = pos[0] - 0.5
 tape_end = pos[0] - 0.5 + l
 
 for i in range(1,n) :
-    if tape_end >= pos[i]+0.5 :
-        continue
-    else :
+    if tape_end < pos[i]+0.5 :
         tape_start = pos[i] - 0.5 # new tape
-    
-    tape_end = tape_start + l
-    count += 1
+        tape_end = tape_start + l
+        count += 1    
 print(count)

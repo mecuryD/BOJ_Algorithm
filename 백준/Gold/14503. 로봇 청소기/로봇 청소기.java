1fc66cea76f9d01f04b7fcde36257f427d7c4695
Bugwang Choe <69384652+mecuryD@users.noreply.github.com>
2023-07-25 01:33:05 +0900

    [Gold V] Title: 로봇 청소기, Time: 132 ms, Memory: 14376 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -1,21 +1,29 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+
 		/* input */
-		int n = sc.nextInt();
-		int m = sc.nextInt();
-		int r = sc.nextInt();
-		int c = sc.nextInt();
-		int d = sc.nextInt();
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		int r = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		int d = Integer.parseInt(st.nextToken());
 		
 		int[][] arr = new int[n][m];
 		for(int i=0; i<n; i++) { // O(N^2)
+			st = new StringTokenizer(br.readLine(), " ");
 			for(int j=0; j<m; j++) {
-				arr[i][j] = sc.nextInt();	
+				arr[i][j] = Integer.parseInt(st.nextToken());	
 			}
 		}
 		

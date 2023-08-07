c6e9bdca199c727bf208f85849df7689a925fb87
Bugwang Choe <69384652+mecuryD@users.noreply.github.com>
2023-08-08 00:28:56 +0900

    [Silver IV] Title: 괄호, Time: 136 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -5,39 +5,34 @@ import java.util.Stack;
 
 public class Main {
 
-	static int T;
-	static String[] lines;
-	static Stack<Character> stack;
+	static int T, cnt;
 	
 	public static void main(String[] args) throws IOException {
 		// 1. 입력
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		T = Integer.parseInt(br.readLine()); // 테스트 케이스
-		lines = new String[T]; // 전체 괄호 문자열 입력
-		for(int i=0; i<T; i++) {
-			lines[i] = br.readLine();
-		}
-		
+
 		// 2. 각 테스트 케이스에 대해 VPS 판별
 		for(int i=0; i<T; i++) { 
+			cnt = 0;
 			String res = "YES";
-			int len = lines[i].length();
-			stack = new Stack<Character>();
+			char[] line = br.readLine().toCharArray();
+			int len = line.length;
 			
 			// 괄호 짝이 맞는지 확인
 			for(int j=0; j<len; j++) {
-				char cur = lines[i].charAt(j);
+				char cur = line[j];
 				if(cur=='(') {
-					stack.push(cur);
-				}else if(stack.isEmpty()) { // 2-1. 여는 괄호가 부족하거나, 닫는 괄호가 많은 경우
-					res = "NO";
-					break;
-				}else {
-					stack.pop();
+					cnt++;
+				}else if(cur==')'){
+					cnt--;
+					if(cnt<0) { // 괄호 짝이 맞지 않다는 것
+						break;
+					}
 				}
 			}
 			
-			if(!stack.isEmpty()) { // 2-2. 닫는 괄호가 부족한 경우
+			if(cnt != 0) { // 2-2. 닫는 괄호가 부족한 경우
 				res = "NO";
 			}
 			System.out.println(res);

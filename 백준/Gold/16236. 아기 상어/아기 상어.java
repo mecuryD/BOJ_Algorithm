7288426b65eb49919d6f0cb6755c48f379bcc57a
Bugwang Choe <69384652+mecuryD@users.noreply.github.com>
2024-01-28 23:13:06 +0900

    [Gold III] Title: 아기 상어, Time: 132 ms, Memory: 14632 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.java
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -1,104 +1,120 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.Comparator;
 import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
-
-	static int N, r, c;
-	static int size, cnt, res;
-	static int[][] map;
-	static int[] dr = {-1, 0, 1, 0}; // 북서남동
-	static int[] dc = {0, -1, 0, 1}; // 북서남동
+// [백준16236] 아기 상어
+public class Main{	
+	static int N; // 공간의 크기
+	static int sharkSize; 	  // 현재 아기 상어의 크기
+	static int sharkEat;      // 현재 아기 상어가 먹은 물고기 수
+	static int sharkTime; // 아기 상어가 움직인 시간
 	
-	public static void input() throws IOException{
+	static int[] sharkStart; // 상어의 출발 위치
+	static int[] dr = new int[] {-1, 0, 1, 0};
+	static int[] dc = new int[] {0, -1, 0, 1};
+
+	static int[][] map; // N x N 공간
+	
+	public static void main(String[] args) throws IOException {
+		// 값 입력
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine()); // 공간 크기 변수
-		map = new int[N][N]; // 공간 저장 배열
+		N = Integer.parseInt(br.readLine());
 		
-		// NxN 공간 입력
+		map = new int[N][N];
 		for(int i=0; i<N; i++) {
-			StringTokenizer stk = new StringTokenizer(br.readLine()," ");
+			StringTokenizer stk = new StringTokenizer(br.readLine(), " ");
 			for(int j=0; j<N; j++) {
-				int tmp = Integer.parseInt(stk.nextToken());
-				map[i][j] = tmp; // 공간 위치 입력
-				if(tmp==9) { // 상어 초기 위치 저장
-					r = i;
-					c = j;
+				map[i][j] = Integer.parseInt(stk.nextToken());
+				if(map[i][j]==9) {
+					sharkStart = new int[] {i, j};
+					map[i][j] = 0;
 				}
 			}
 		}
+		
+		// 아기 상어 초기화
+		sharkEat = 0;
+		sharkSize = 2;
+		sharkTime = 0;
+		
+		// 아기 상어가 움직이면서 먹을 수 있는 물고기를 찾는다
+		Queue<Fish> queue = new ArrayDeque<Fish>();
+		boolean[][] visited = new boolean[N][N];
+		
+		queue.offer(new Fish(sharkStart[0], sharkStart[1], 0));
+		visited[sharkStart[0]][sharkStart[1]] = true;
+		
+		PriorityQueue<Fish> pq = new PriorityQueue<Fish>(new Comparator<Fish>() {
+			@Override
+			public int compare(Fish o1, Fish o2) {
+				if(o1.r == o2.r) return (o1.c - o2.c); 
+				else return (o1.r - o2.r);
+			}
+		});
+		
+		while(!queue.isEmpty()) {
+			// 동일한 타임 뎁스만 큐에서 뽑는다
+			int size = queue.size();
+			for(int i=0; i<size; i++) {
+				// 상어 위치를 뽑는다
+				Fish cur = queue.poll();
+				// 현재 상어 위치에서 상하좌우를 살펴, 먹을 수 있는 물고기를 찾는다
+				for(int j=0; j<4; j++) {
+					int nr = cur.r + dr[j];
+					int nc = cur.c + dc[j];
+					if(nr<0 || nc<0 || nr>=N || nc>=N) continue;	  		 // 맵 범위 이탈
+					if(visited[nr][nc] || sharkSize < map[nr][nc]) continue; // 이미 방문한 곳이거나, 아기 상어보다 큰 물고기
+					if(0 < map[nr][nc] && map[nr][nc] < sharkSize) {  		 // 먹을 수 있는 물고기가 발견되면
+						pq.offer(new Fish(nr, nc, cur.t+1));		  		 // 우선순위 큐에 넣는다
+					}
+					
+					queue.offer(new Fish(nr, nc, cur.t+1));
+					visited[nr][nc] = true;
+				}
+			}
+			
+			// 만약 먹을 수 있는 물고기가 있다면
+			if(!pq.isEmpty()) {
+				// 물고기를 먹는다
+				Fish fish = pq.poll();
+				map[fish.r][fish.c] = 0;
+				
+				sharkEat++;
+				sharkTime = fish.t;
+				
+				queue.clear();
+				queue.offer(new Fish(fish.r, fish.c, fish.t));
+				visited = new boolean[N][N];
+
+				// 아기 상어가 성장했는지 확인한다
+				if(sharkEat==sharkSize) {
+					sharkEat=0;
+					sharkSize++;
+				}
+			}
+			
+			// 우선순위 큐 초기화
+			pq.clear();
+		}
+		
+		// 결과 출력
+		System.out.println(sharkTime);
 	}
 	
-	public static void main(String[] args) throws IOException{
-		// 값 입력
-		input(); // NxN 공간 입력
-		size = 2; // 상어의 최초 크기
-		cnt = 0; // 상어가 지금까지 몇 마리를 먹었는지 저장
-
-		while(true) { // 새로운 위치에서 시작 (Queue, visited 초기화)
-			PriorityQueue<int[]> q = new PriorityQueue<int[]>( // 우선순위 큐 생성
-				new Comparator<int[]>() {
-					@Override
-					public int compare(int[] o1, int[] o2) {
-						if(o1[2]!=o2[2]) { // 거리가 같지 않으면
-							return o1[2]-o2[2]; // 거리 따라 오름차순
-						}else {	// 거리가 같으면
-							if(o1[0]!=o2[0]) { // 거리는  같고, 행이 같지 않으면
-								return o1[0]-o2[0]; // 행에 따라 오름차순
-							}else { // 거리와 행이 모두 같으면
-								return o1[1]-o2[1]; // 열에 따라 오름차순 정렬
-							}
-						}
-					} 
-				}
-			);
-			boolean[][] visited = new boolean[N][N]; // 방문 배열
-			
-			q.offer(new int[] {r,c,0}); // 상어 현재 위치 추가
-			map[r][c] = 0; // 상어의 초기 위치 표시 해제
-			visited[r][c] = true; // 상어의 초기 위치 방문 체크 
-			
-			// BFS 탐색
-			int isExist = 0;
-			while(!q.isEmpty()) { // 큐가 빌 때까지 반복
-				int[] cur = q.poll(); // 탐색할 위치를 꺼낸다
-				if(map[cur[0]][cur[1]]!=0 && map[cur[0]][cur[1]]<size) { // 먹을 수 있는 물고기라면
-					map[cur[0]][cur[1]] = 0; // 물고기를 먹는다
-					res += cur[2]; // 총 거리에 반영
-					isExist++; // 먹은 물고기의 수 갱신
-					r = cur[0]; // 상어의 위치 업데이트
-					c = cur[1]; // 상어의 위치 업데이트
-//					System.out.println("r : "+r+", c : "+c);
-					break;
-				}
-				for(int i=0; i<4; i++) { // 4방향 탐색 (북서남동)
-					int nr = cur[0] + dr[i]; // 다음에 탐색할 행
-					int nc = cur[1] + dc[i]; // 다음에 탐색할 열
-					if(nr<0 || nc<0 || nr>=N || nc>=N) continue; // 범위 초과 시 continue
-					if(visited[nr][nc]) continue; // 이미 방문한 곳(=큐에 삽입된 곳)이면 continue
-					if(map[nr][nc]>size) continue; // 상어보다 큰 물고기인 경우 continue
-					q.offer(new int[] {nr,nc,cur[2]+1}); // 이동 가능한 곳이면 큐에 삽입
-					visited[nr][nc] = true; // 큐에 다시 삽입되지 않도록 방문 처리
-				}
-			};
-			
-			// TODO : 먹을 수 있는 물고기가 없다는 것을 어떻게 판단할까  -> 새로운 변수
-			if(isExist==0) { // 가능한 모든 곳을 탐색했음에도 물고기를 먹지 못한 것이므로
-				break; // 반복문 종료
-			}
-			
-			cnt += isExist; // 방금 먹은 물고기 마리 수 반영
-//			System.out.println("cnt : "+cnt+", size : " + size);
-			if(cnt>=size) { // 상어가 자기 크기만큼 물고기를 먹었다면
-				size++; // 크기 증가
-				cnt=0; // 먹은 물고기 수 초기화
-			}
-			
-			
+	static class Fish {
+		int r;
+		int c;
+		int t;
+		
+		public Fish(int r, int c, int t) {
+			this.r = r;
+			this.c = c;
+			this.t = t;
 		}
-		System.out.println(res);
 	}
 }

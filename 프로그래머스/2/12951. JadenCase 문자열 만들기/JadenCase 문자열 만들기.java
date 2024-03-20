41aa7669927aa0bf431724a55a4560635f3c3342
Bugwang Choe <69384652+mecuryD@users.noreply.github.com>
2024-03-20 21:20:02 +0900

    Update JadenCase 문자열 만들기.java

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,3 +1,6 @@
+// JadenCase 문자열 만들기
+// Java에서 제공하는 함수를 충분히 활용하자
+// Java에서는 모든 문자를 대/소문자로 변환하는 toUpperCase(), toLowerCase()를 제공한다
 class Solution {
     public String solution(String s) {
         String answer = "";

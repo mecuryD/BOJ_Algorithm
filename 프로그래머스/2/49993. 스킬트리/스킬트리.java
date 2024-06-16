17dfc4c03b861c4c700f0c7c8c695b7e7ae4d94c
Bugwang Choe <69384652+mecuryD@users.noreply.github.com>
2024-06-16 17:11:53 +0900

    [level 2] Title: 스킬트리, Time: 0.13 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -21,34 +21,27 @@ class Solution {
         
         // 각 스킬 트리를 살펴보며 가능한지 체크
         for(String skillTree : skill_trees){
-            if(isPossible(skillTree)){
+            int skillIdx = 0;
+            int lenSkillTree = skillTree.length();
+            boolean isPossible = true;
+            
+            for(int i=0; i<lenSkillTree; i++){
+                int skillTreeIdx = skill.indexOf(skillTree.charAt(i));
+                if(skillTreeIdx == -1){
+                    continue;
+                }else if(skillTreeIdx == skillIdx){
+                    skillIdx++;
+                }else{
+                    isPossible = false;
+                    break;
+                }
+            }
+            
+            if(isPossible){
                result++; 
             }
         }
         
         return result;
-    }
-    
-    
-    boolean isPossible(String skillTree){
-        // 스킬 트리를 문자 배열로 변환
-        char[] charSt = skillTree.toCharArray();
-        int lenSt = charSt.length;
-        int idx = 0;
-        
-        // 각 문자 배열이 큐에 존재하는 지 확인하고, 존재하면 피크 값과 비교한다
-        for(int i=0; i<lenSt; i++){
-            if(skillOrder.contains(charSt[i])){
-                // 선행 스킬 학습이 필요하다면
-                if(skillOrder.get(idx) != charSt[i]){
-                    return false;
-                }
-                
-                // 선행 스킬 학습이 이미 되어있다면
-                idx++;
-            }
-        }
-        
-        return true;
     }
 }

4c2aa3d7d825cda5120bd363a182d9a82b81fb96
Bugwang Choe <69384652+mecuryD@users.noreply.github.com>
2024-07-03 02:38:17 +0900

    [level 2] Title: NULL 처리하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
+++ 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -1,5 +1,4 @@
--- 동물의 생물 종, 이름, 성별 및 중성화 여부 조회
--- 아이디 순으로 조회
--- 이름이 없는 동물의 이름은 No name 표시
-SELECT ANIMAL_TYPE, IFNULL(NAME,'No name') 'NAME', SEX_UPON_INTAKE
-FROM ANIMAL_INS;
+-- 이름이 없는 동물의 이름은 No name
+SELECT ANIMAL_TYPE, IFNULL(NAME, 'No name') as NAME, SEX_UPON_INTAKE
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID

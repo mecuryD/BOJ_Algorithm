bff16efc7fa3da730f718bde4d92bd389a75785c
Bugwang Choe <69384652+mecuryD@users.noreply.github.com>
2024-07-08 14:21:09 +0900

    [level 2] Title: 타겟 넘버, Time: 4.15 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 // [프로그래머스] 타겟 넘버
 class Solution {
     
@@ -5,12 +7,13 @@ class Solution {
     
     public int solution(int[] numbers, int target) {
         int n = numbers.length;
-        dfs(n, target, 0, 0, numbers);
+        // dfs(n, target, 0, 0, numbers);
+        bfs(n, target, numbers);
         return result;
     }
     
     
-    // DFS 완전 탐색
+    // DFS 완전 탐색 (17분)
     public void dfs(int n, int target, int idx, int sum, int[] numbers){
         if(idx==n){
             if(sum==target){
@@ -25,4 +28,25 @@ class Solution {
         // idx번째 숫자를 뺀다
         dfs(n, target, idx + 1, sum - numbers[idx], numbers);
     }
+    
+    // BFS 완전 탐색
+    public void bfs(int n, int target, int[] numbers){
+        Queue<int[]> queue = new ArrayDeque<int[]>();
+        queue.offer(new int[]{0, numbers[0]});
+        queue.offer(new int[]{0, (-1) * numbers[0]});
+        
+        while(!queue.isEmpty()){
+            int[] cur = queue.poll();
+            if(cur[0]==n-1){
+                if(cur[1]==target){
+                    result++;
+                }
+                continue;
+            }
+            
+            queue.offer(new int[]{cur[0] + 1, cur[1] + numbers[cur[0] + 1]});
+            queue.offer(new int[]{cur[0] + 1, cur[1] - numbers[cur[0] + 1]});
+        }
+    }
+    
 }

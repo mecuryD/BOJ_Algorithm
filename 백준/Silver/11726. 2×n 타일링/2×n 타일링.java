8aeebcc878752adf450d02005342b1750b835f3c
Bugwang Choe <69384652+mecuryD@users.noreply.github.com>
2024-08-01 20:56:46 +0900

    [Silver III] Title: 2×n 타일링, Time: 132 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,21 +1,26 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
-// [BOJ11726] 2xn 타일링
+import java.io.InputStreamReader;
+
+// [백준] 2xN 타일링
 public class Main {
-	public static void main(String[] args) throws IOException{
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt(); // 2xn 직사각형
-		int[] dp = new int[N+1]; // dp-table
-		if(N==1) {
-			System.out.println(1);
-		}else {			
-			dp[1] = 1;
-			dp[2] = 2;
-			for(int i=3; i<=N; i++) {
-				// 오버플로우를 대비하여 10007로 미리 나눠줘야 한다
-				dp[i] = (dp[i-1] + dp[i-2]) % 10007;
-			}
-			System.out.println(dp[N]);
+
+	public static void main(String[] args) throws IOException {
+		// 입력
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		// 테이블 정의
+		int[] table = new int[N+1];
+		table[1] = 1;
+		if(N>1) table[2] = 2;
+		
+		// 다이나믹 프로그래밍
+		for(int i=3; i<=N; i++) {
+			table[i] = (table[i-1] + table[i-2]) % 10007;
 		}
+		
+		// 결과 출력
+		System.out.print(table[N]);
 	}
 }

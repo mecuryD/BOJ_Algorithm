7ff4f7bc8a2fb88a5e2f3d3af9b47702d8e86dcb
Bugwang Choe <69384652+mecuryD@users.noreply.github.com>
2024-08-01 19:44:18 +0900

    [Silver III] Title: 계단 오르기, Time: 140 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -2,28 +2,31 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-// [BOJ2579] 계단 오르기
-public class Main{
+// [백준] 계단 오르기
+public class Main {
+
 	public static void main(String[] args) throws IOException {
-		// 입력 초기화
+		// 입력
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine()); // 계단의 개수
-		int[] arr = new int[N+1]; // 각 계단의 점수
-		for(int i=1; i<=N; i++) { 
-			arr[i] = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] S = new int[N+1];
+		for(int i=1; i<=N; i++) {
+			S[i] = Integer.parseInt(br.readLine());
 		}
 		
-		// Bottom-Up DP
-		int[] dp = new int[N+1]; // dp-table
-        dp[1] = arr[1]; // 첫 번째 계단
-        if(N>=2) { // 두 번째 계단
-        	dp[2] = arr[1] + arr[2];
-        }
-        
-		for(int i=3; i<=N; i++) {
-			dp[i] = arr[i] + Math.max(dp[i-2], arr[i-1] + dp[i-3]);
+		// 테이블 정의
+		int[] table = new int[N+1];
+		table[1] = S[1];
+		if(N>1) table[2] = S[1] + S[2];
+		if(N>2) table[3] = Math.max(S[1], S[2]) + S[3];
+		
+		// 다이나믹 프로그래밍
+		for(int i=4; i<=N; i++) {
+			table[i] = Math.max(S[i-1] + table[i-3], table[i-2]) + S[i];
 		}
-			
-		System.out.println(dp[N]);
+		
+		// 결과 출력
+		System.out.print(table[N]);
 	}
 }

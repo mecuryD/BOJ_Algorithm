4d4c4289ddfe9d4c711f317e6b760c1a4e94b916
Bugwang Choe <69384652+mecuryD@users.noreply.github.com>
2024-08-01 19:09:07 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 124 ms, Memory: 14084 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,22 +1,39 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
+// [백준] 1,2,3 더하기
 public class Main{
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine()); // 테스트 케이스 개수
-		int[] table = new int[11]; // dp-table
-		// Bottom-up Table
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		
+		// 테이블 정의
+		int[] table = new int[11];
 		table[1] = 1;
 		table[2] = 2;
 		table[3] = 4;
-		for(int i=4; i<11; i++) 
-			table[i] = table[i-1] + table[i-2] + table[i-3];
 		
-		for(int i=0; i<T; i++) {
+		// 다이나믹 프로그래밍
+		for(int i=4; i<=10; i++) {
+			if(i>1) table[i] += table[i-1];
+			if(i>2) table[i] += table[i-2];
+			if(i>3) table[i] += table[i-3];
+		}
+		
+		// 테스트케이스 반복
+		for(int t=0; t<T; t++) {
+			// 입력
 			int N = Integer.parseInt(br.readLine());
-			System.out.println(table[N]);
-		}	
+			
+			// 결과 저장
+			sb.append(table[N]).append("\n");
+		}
+		
+		// 결과 출력
+		System.out.print(sb);
 	}
 }

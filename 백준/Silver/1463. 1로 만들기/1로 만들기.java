d7c955cecc2c34e492ea32d11d3eeb05ad6fa763
Bugwang Choe <69384652+mecuryD@users.noreply.github.com>
2024-08-01 18:41:55 +0900

    [Silver III] Title: 1로 만들기, Time: 160 ms, Memory: 18136 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,28 +1,34 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
-// [BOJ1463] 1로 만들기
-public class Main {
-	public static void main(String[] args){
-		// 입력 초기화
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt(); // 정수 N
-		int[] dp = new int[N+1]; // DP Table
+// [백준] 1로 만들기
+public class Main{
+
+	public static void main(String[] args) throws IOException {
+		// 입력
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
 		
-		// Bottom-Up DP
-		int min;
-		for(int i=2; i<=N; i++) {
-			min = dp[i-1]+1; // 1을 뺀다
+		// 테이블 정의 및 초기값 설정
+		int[] table = new int[N+1];
+		Arrays.fill(table, 999999);
+		table[1] = 0;
+		
+		// 다이나믹 프로그래밍
+		for(int i=1; i<=N; i++) {
+			// 3으로 나눌 수 있으면 3으로 나눠본다
+			if(i % 3 == 0) table[i] = Math.min(table[i], table[i/3] + 1);
 			
-			if(i%2==0) // X가 2로 나누어 떨어지면,  2로 나눈다
-				min = Math.min(min, dp[i/2]+1);
+			// 2로 나눌 수 있으면 2로 나눠본다
+			if(i % 2 == 0) table[i] = Math.min(table[i], table[i/2] + 1);
 			
-			if(i%3==0) // X가 3으로 나누어 떨어지면, 3으로 나눈다
-				min = Math.min(min, dp[i/3]+1);
-			
-			dp[i] = min; // i값을 1로 만드는 최소 연산 횟수 저장
+			// 1을 뺀다
+			table[i] = Math.min(table[i], table[i-1] + 1);
 		}
 		
 		// 결과 출력
-		System.out.println(dp[N]);
+		System.out.print(table[N]);
 	}
 }

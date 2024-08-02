65e4f2b3406bbd0605a53f0654ce3562cb46e51b
Bugwang Choe <69384652+mecuryD@users.noreply.github.com>
2024-08-02 10:44:43 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 668 ms, Memory: 56036 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,48 +1,42 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+// [백준] 구간 합 구하기 4
+public class Main{
 
-	static int N, M; // 수의 개수 N, 합을 구해야 하는 횟수 M
-	static int res; // 최종 결과를 담는 변수
-	static int start, end; // 합을 구해야 하는 구간의 시작과 끝
-	static int[] arr, table; // table[0]부터 table[i]까지의 누적합을 담는 배열
-	
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
+		// 입력
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stk = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(stk.nextToken());
+		int M = Integer.parseInt(stk.nextToken());
 		
-		// 값 입력
-		StringTokenizer stk = new StringTokenizer(br.readLine(), " ");
-		N = Integer.parseInt(stk.nextToken());
-		M = Integer.parseInt(stk.nextToken());
-		
-		arr = new int[N];
-		table = new int[N];
-
-		stk = new StringTokenizer(br.readLine(), " ");
-		for(int i=0; i<N; i++) {
-			arr[i] = Integer.parseInt(stk.nextToken());
-			if(i==0) {
-				table[i] = arr[i];
-			}else {
-				table[i] = arr[i] + table[i-1];
-			}
+		int[] numbers = new int[N+1];
+		stk = new StringTokenizer(br.readLine());
+		for(int i=1; i<=N; i++) {
+			numbers[i] = Integer.parseInt(stk.nextToken());
 		}
 		
-
-		// table을 사용하여 구간 합 출력
+		// 테이블 정의
+		int[] table = new int[N+1];
+		table[1] = numbers[1];
+		
+		// 다이나믹 프로그래밍
+		for(int i=2; i<=N; i++) {
+			table[i] += table[i-1] + numbers[i];
+		}
+		
+		StringBuilder sb = new StringBuilder();
 		for(int i=0; i<M; i++) {
-			// 구간 정보 입력
-			stk = new StringTokenizer(br.readLine(), " ");
-			start = Integer.parseInt(stk.nextToken())-1;
-			end = Integer.parseInt(stk.nextToken())-1;
-			
-			// 누적 합 출력
-			res = (start==0)? table[end] : table[end]-table[start-1];
-			System.out.println(res);
+			stk = new StringTokenizer(br.readLine());
+			int start =  Integer.parseInt(stk.nextToken());
+			int end = Integer.parseInt(stk.nextToken());
+			sb.append(table[end]-table[start-1]).append("\n");
 		}
+		
+		// 결과 출력
+		System.out.print(sb);
 	}
 }

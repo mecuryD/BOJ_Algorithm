11b9b175981d0e57b96af7e0add94b3ff62ee048
Bugwang Choe <69384652+mecuryD@users.noreply.github.com>
2024-08-06 23:55:32 +0900

    [level 3] Title: 가장 먼 노드, Time: 38.07 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,48 +1,79 @@
+// 다익스트라
 import java.util.*;
 
-// [프로그래머스] 가장 먼 노드
 class Solution {
+
+    static class Node implements Comparable<Node> {
+        int number, cost;
+
+        public Node(int number, int cost) {
+            this.number = number;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Node n) {
+            return cost - n.cost;
+        }
+    }
+
+    int n;
+    int[][] edge;
+    List<Integer>[] graph;
+    int farDistance;
+
     public int solution(int n, int[][] edge) {
-        // 그래프 생성
-        ArrayList<Integer>[] graph = new ArrayList[n+1];
-        for(int i=1; i<=n; i++){
-            graph[i] = new ArrayList<Integer>();
+        this.n = n;
+        this.edge = edge;
+        init();
+
+        int[] dist = dijkstra();
+
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            if (dist[i] == farDistance) {
+                answer++;
+            }
         }
-        
-        for(int i=0; i<edge.length; i++){
-            graph[edge[i][0]].add(edge[i][1]);
-            graph[edge[i][1]].add(edge[i][0]);
-        }
-        
-        // BFS 탐색
-        int[] distance = new int[n+1];
-        Arrays.fill(distance, 99999);
-        distance[1] = 0;
-        int maxDis = 0;
-        
-        Queue<int[]> queue = new ArrayDeque<int[]>();
-        queue.offer(new int[]{1, 0});
-        
-        while(!queue.isEmpty()){
-            // 큐에서 노드 하나를 꺼낸다
-            int[] cur = queue.poll();
-            
-            // 아직 방문하지 않은 노드만 큐에 삽입한다
-            for(int node : graph[cur[0]]){
-                if(distance[node] == 99999){
-                    distance[node] = cur[1] + 1;
-                    queue.offer(new int[]{node, distance[node]});
-                    maxDis = (maxDis < distance[node]) ? distance[node] : maxDis;
+
+        return answer;
+    }
+
+    public int[] dijkstra() {
+        int[] dist = new int[n + 1];
+        Arrays.fill(dist, 1000000);
+        dist[1] = 0;
+        PriorityQueue<Node> que = new PriorityQueue<>();
+        que.offer(new Node(1, 0));
+
+        while (!que.isEmpty()) {
+            Node current = que.poll();
+
+            if (dist[current.number] < current.cost) {
+                continue;
+            }
+
+            for (int next : graph[current.number]) {
+                if (dist[next] > current.cost + 1) {
+                    dist[next] = current.cost + 1;
+                    farDistance = dist[next];
+                    que.offer(new Node(next, dist[next]));
                 }
             }
         }
-        
-        // 가장 먼 노드의 개수를 구한다
-        int count = 0;
-        for(int i=1; i<=n; i++){
-            if(distance[i]==maxDis) count++;
+
+        return dist;
+    }
+
+    public void init() {
+        graph = new ArrayList[n + 1];
+        for (int i = 0; i <= n; i++) {
+            graph[i] = new ArrayList<>();
         }
-        
-        return count;
+
+        for (int i = 0; i < edge.length; i++) {
+            graph[edge[i][0]].add(edge[i][1]);
+            graph[edge[i][1]].add(edge[i][0]);
+        }
     }
 }

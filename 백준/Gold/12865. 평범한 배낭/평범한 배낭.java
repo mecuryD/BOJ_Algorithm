6c7ee011f66809b62bb1892887e06a2d3f4d1e70
Bugwang Choe <69384652+mecuryD@users.noreply.github.com>
2024-08-26 21:49:32 +0900

    [Gold V] Title: 평범한 배낭, Time: 188 ms, Memory: 55260 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -3,37 +3,37 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-// [BOJ12865] 평범한 배낭
+// [백준] 평범한 배낭
+public class Main {
 
-public class Main{
-
-	public static void main(String[] args) throws IOException{
-		// 입력 초기화
+	public static void main(String[] args) throws IOException {
+		// 입력
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer stk = new StringTokenizer(br.readLine(), " ");
+		StringTokenizer stk = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(stk.nextToken());
+		int K = Integer.parseInt(stk.nextToken());
 		
-		int N = Integer.parseInt(stk.nextToken()); // 물품의 수 N
-		int K = Integer.parseInt(stk.nextToken()); // 준서가 버틸 수 있는 무게 K
-		int[] W = new int[N+1]; // 각 물품의 무게 W
-		int[] V = new int[N+1]; // 각 물품의 가치 V
+		// 테이블 정의
+		int[][] table = new int[N+1][K+1];
 		for(int i=1; i<=N; i++) {
-			stk = new StringTokenizer(br.readLine(), " ");
-			W[i] = Integer.parseInt(stk.nextToken());
-			V[i] = Integer.parseInt(stk.nextToken());
-		}
-		
-		// Bottom-up DP
-		int[][] table = new int[N+1][K+1]; // 동적 테이블
-		for(int i=1; i<=N; i++) { // 1번째 ~ i번째 물건 고려
-			for(int j=1; j<=K; j++) { // 1kg ~ jkg 배낭 고려
-				if(j < W[i]) // i번째 물건을 현재 배낭에 넣을 수 없다
+			stk = new StringTokenizer(br.readLine());
+			int W = Integer.parseInt(stk.nextToken());
+			int V = Integer.parseInt(stk.nextToken());
+			
+			// 다이나믹 프로그래ㅐ밍
+			for(int j=1; j<=K; j++) {
+				if(j<W) {
+					// i번째 물건의 무게가 배낭 용량 j보다 작은 경우
 					table[i][j] = table[i-1][j];
-				else // i번쨰 물건을 현재 배낭에 넣을 수 있다
-					table[i][j] = Math.max(table[i-1][j], V[i] + table[i-1][j-W[i]]);
+				}else {
+					// i번째 물건의 무게가 배낭 용량 j보다 같거나 큰 경우
+					// i번째 물건을 담는 경우, 담지 않는 경우를 고려하여 가치가 더 큰 값 선택
+					table[i][j] = Math.max(table[i-1][j], table[i-1][j-W] + V);
+				}
 			}
 		}
-	
-		// 결과 출력
-		System.out.println(table[N][K]);
+		
+		System.out.print(table[N][K]);
 	}
+
 }
